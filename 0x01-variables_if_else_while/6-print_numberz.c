28b9592ed43c62275adfe92602ff1843e5c8fafc
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-14 16:26:04 -0700

    Pint numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Program starts here
+ * main - The program starts here
  * Description: Prints single-digit numbers of base 10 starting from 0
  * Return: Always0(Success)
  */
 
 int main(void)
 {
-	int number;
-{
+int number;
 for (number = 0; number < 10; number++)
-	putchar(number + '0');
-	putchar('\n');
+{
+putchar(number + '0');
 }
+putchar('\n');
 return (0);
 }
 

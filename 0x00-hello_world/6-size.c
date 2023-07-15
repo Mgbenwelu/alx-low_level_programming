a569e5a273d9c7fe94ec8048db68694ff5247454
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-14 19:33:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Program starts here
+ * main - printing size of various computer types
  * Return: 0 (success)
  */
 
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long: %zu byte(s)\n", sizeof(long));
-printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-printf("Size of void *: %zu byte(s)\n", sizeof(void *));
-printf("Size of int *: %zu byte(s)\n", sizeof(int *));
-printf("Size of long *: %zu byte(s)\n", sizeof(long *));
-printf("Size of char *: %zu byte(s)\n", sizeof(char *));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long) sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
-

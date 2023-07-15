5e08e070d9e1aa658f8a131a8d45a0ec04e8b4b9
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-14 18:29:26 -0700

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * main -main block
+ * main - Program starts here
  *Return: 0
  */
+
 int main(void)
 {	puts("\"with proper grammar, but the outcome is a piece of art");
 	return (0);

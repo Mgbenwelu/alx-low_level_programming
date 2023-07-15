2bc6b43b3551521ca0c01f378940a3306bdc621e
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-14 18:32:58 -0700

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 /**
  * main - Program starts here
- *Return: 0
+ * Return: 0 (success)
  */
 
 int main(void)
-{	puts("\"with proper grammar, but the outcome is a piece of art");
-	return (0);
+{
+printf("%s\n", "\"with proper grammar, but the outcome is a piece of art");
+return (0);
 }
+

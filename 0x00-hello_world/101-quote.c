93402a3c99851d3cfa35e8ba560021689629e149
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-17 18:05:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Program starts here
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+write(STDERR_FILENO, "and that piece of art is useful\" - ", 36);
+write(STDERR_FILENO, "Dora Korpar, 2015-10-19\n", 25);
+return (1);
+}
 

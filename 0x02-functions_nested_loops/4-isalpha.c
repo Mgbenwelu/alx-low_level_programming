65498366bae8c54d890bc2410f35987f1a86d87b
mgbenwelu <mgbenwelumichael@gmail.com>
2023-07-18 10:08:52 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Checks for alphabetic order
+ *_isalpha - Checks for alphabetic order
  @c: alpabet to check for
  * Return: 1 if c is a letter, 0 otherwise
  */
@@ -8,5 +8,4 @@ int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-
 
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,8 +3,9 @@
  * print_last_digit - prints the last digit of a number
  *
  * @n: the int to extract the last digit from
- * 
+ *
  * Return: value of the last digit
+ *
  */
 int print_last_digit(int n)
 {

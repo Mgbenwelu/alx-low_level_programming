36066899df741ed911736fcd2c93aa90f07ad83d
Chidi Mgbenwelu <mgbenwelumichael@gmail.com>
2023-10-17 16:56:07 -0700

    push check

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile - This reads a text file and prints the letters
- * @filename: the filename
- * @letters: Numbers of letters printed
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
  * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
